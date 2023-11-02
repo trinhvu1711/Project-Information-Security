4437009e56c3e8fdaeabe28c9ef8edc6ab8b0c81
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2023-11-02 21:45:26 +0700

    add twofish model

--- src/main/java/models/Twofish.java
+++ src/main/java/models/Twofish.java
@@ -1,4 +1,64 @@
 package models;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-public class Twofish {
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.Base64;
+
+public class Twofish implements EncryptionAlgorithm  {
+    private SecretKey key;
+
+    public Twofish() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public void generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator keygen = KeyGenerator.getInstance("Twofish");
+        key = keygen.generateKey();
+    }
+    @Override
+    public String calculate(String plainText) {
+        byte[] result;
+        Cipher encrypt = null;
+        try {
+            encrypt = Cipher.getInstance("Twofish");
+            encrypt.init(Cipher.ENCRYPT_MODE, key);
+            result = encrypt.doFinal(plainText.getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return Base64.getEncoder().encodeToString(result);
+    }
+
+    @Override
+    public String decrypt(String plainText) {
+        byte[] result;
+        try {
+            byte[] text = Base64.getDecoder().decode(plainText);
+            Cipher encrypt = Cipher.getInstance("Twofish");
+            encrypt.init(Cipher.DECRYPT_MODE, key);
+            result = encrypt.doFinal(text);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return new String(result);
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Twofish twofish = new Twofish();
+        twofish.generateKey();
+        System.out.println(twofish.key);
+        byte[] keyBytes = twofish.key.getEncoded();
+        System.out.println("Key: " + keyBytes);
+        System.out.println("Key: " + Base64.getEncoder().encodeToString(keyBytes));
+        String text = "vudeptrai";
+        String encrypt = twofish.calculate(text);
+        String decrypt = twofish.decrypt(encrypt);
+        System.out.println(encrypt);
+        System.out.println(decrypt);
+    }
 }
