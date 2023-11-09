1d105a207771dde45d4f2acfdb93af926ab2b32f
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2023-11-09 23:50:14 +0700

    add hmac hash function

--- src/main/java/models/MD5.java
+++ src/main/java/models/MD5.java
@@ -1,11 +1,14 @@
 package models;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MD5 implements EncryptionAlgorithm {
-
+    private SecretKey key;
     @Override
     public String calculate(String inputText) {
         MessageDigest md = null;
@@ -23,7 +26,16 @@ public class MD5 implements EncryptionAlgorithm {
     public String decrypt(String inputText) {
         return null;
     }
-
+    public void generateKey(String algorithm) throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance(algorithm);
+        key = keyGen.generateKey();
+    }
+    public String hmacHashFunction(String algorithm, String data)
+            throws Exception {
+        Mac mac = Mac.getInstance(algorithm);
+        mac.init(key);
+        return VietnameseTextHelper.bytesToHex(mac.doFinal(data.getBytes()));
+    }
     public static void main(String[] args) {
         MD5 md5 = new MD5();
         System.out.println(md5.calculate("vudeptrai"));
